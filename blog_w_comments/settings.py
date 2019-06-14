f39f18f1538ed2218e2716bbe669f37a2833dc2f
shihab <echoshihab@gmail.com>
2019-06-13 21:22:15 -0400

    configure working password reset email via sendinblue.com transactional email'

--- blog_w_comments/settings.py
+++ blog_w_comments/settings.py
@@ -135,4 +135,11 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+EMAIL_HOST = 'smtp-relay.sendinblue.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
