cb7ebd28ed1f077f7875c0a220df359b3a6685ef
shihab <echoshihab@gmail.com>
2019-06-14 21:03:07 -0400

    add ability to edit and delete posts'

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import PostListView
+from .views import (
+    PostListView,
+    PostUpdateView,
+    PostDetailView,
+    PostDeleteView,
+)
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
+    path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('<int:pk>/delete', PostDeleteView.as_view(), name='post_delete'),
+    path('', PostListView.as_view(), name='post_list'),
 ]
+0
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,26 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 
 class PostListView(ListView):
     model = Post
     template_name = 'post_list.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('title', 'content')
+    template_name = 'post_edit.html'
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
