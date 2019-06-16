ba325dc342457a4486864c01232213dd8a3d4109
shihab <echoshihab@gmail.com>
2019-06-16 16:11:48 -0400

    add comments via backend and show comments'

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 # Register your models here.
 
-admin.site.register(Post)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
